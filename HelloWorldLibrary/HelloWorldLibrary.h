b931e66f315b027511d1ad75d8255b08d668df5c
Alexandre Cools <hello@alexandrecools.com>
2020-01-14 19:08:50 +0100

    Initial Commit

---
+++ HelloWorldLibrary/HelloWorldLibrary.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldLibrary.h
+//  HelloWorldLibrary
+//
+//  Created by Alexandre Cools on 14/01/2020.
+//  Copyright © 2020 Alexandre Cools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldLibrary.
+FOUNDATION_EXPORT double HelloWorldLibraryVersionNumber;
+
+//! Project version string for HelloWorldLibrary.
+FOUNDATION_EXPORT const unsigned char HelloWorldLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldLibrary/PublicHeader.h>
+
+
