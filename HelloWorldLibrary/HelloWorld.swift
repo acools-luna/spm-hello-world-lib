54c27a00141770dde6c1a923e8d5cc322f2a7ffb
Alexandre Cools <hello@alexandrecools.com>
2020-01-14 19:11:29 +0100

    Add say function

---
+++ HelloWorldLibrary/HelloWorld.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld.swift
+//  HelloWorldLibrary
+//
+//  Created by Alexandre Cools on 14/01/2020.
+//  Copyright © 2020 Alexandre Cools. All rights reserved.
+//
+
+import Foundation
+
+class HelloWorld {
+    
+    open func sayHello(to name: String) {
+        print("Hello \(name)")
+    }
+    
+}
